0eadae22a88cacbd4b0bc56f943eb6377cd95013
prometheusalpha <hoangnguyen.hn212.hn@gmail.com>
2022-11-08 16:12:47 +0700

    CHANGE update cards

--- pages/collection/[id]/EditFlashCard.tsx
+++ pages/collection/[id]/EditFlashCard.tsx
@@ -1,5 +1,5 @@
 import { DocumentData } from "firebase/firestore";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 
 interface S {
   info: DocumentData;
@@ -9,12 +9,28 @@ interface S {
 }
 
 export default function EditFlashCard(props: S) {
-  const questionRef = useRef<HTMLInputElement>(null);
-  const answerRef = useRef<HTMLInputElement>(null);
+  const questionRef = useRef<HTMLTextAreaElement>(null);
+  const answerRef = useRef<HTMLTextAreaElement>(null);
+
+  useEffect(() => {
+    let newQuestion = questionRef.current?.value;
+    let newAnswer = answerRef.current?.value;
+    let questionLength = newQuestion?.split("\n").length || 0;
+    let answerLength = newAnswer?.split("\n").length || 0;
+
+    if (questionRef.current) {
+      questionRef.current.rows = questionLength;
+    }
+
+    if (answerRef.current) {
+      answerRef.current.rows = answerLength;
+    }
+  }, [questionRef.current?.value, answerRef.current?.value]);
 
   const update = () => {
     let newQuestion = questionRef.current?.value;
     let newAnswer = answerRef.current?.value;
+
     props.updateFlashCard({
       id: props.id,
       question: newQuestion,
@@ -30,22 +46,20 @@ export default function EditFlashCard(props: S) {
       </p>
       <div className="grid gap-6 px-4 py-6 lg:grid-cols-2">
         <p className="max-w-[100%]">
-          <input
-            type="text"
-            className="block w-full border-b bg-gray-700 focus:border-blue-500 focus:outline-none"
+          <textarea
+            className="block w-full resize-none border-b bg-gray-700 focus:border-blue-500 focus:outline-none"
             ref={questionRef}
             onChange={update}
             value={props.info?.question}
-          />
+          ></textarea>
         </p>
         <p className="max-w-[100%]">
-          <input
-            type="text"
-            className="block w-full border-b bg-gray-700 focus:border-blue-500 focus:outline-none"
+          <textarea
+            className="block w-full resize-none border-b bg-gray-700 focus:border-blue-500 focus:outline-none"
             ref={answerRef}
             onChange={update}
             value={props.info?.answer}
-          />
+          ></textarea>
         </p>
       </div>
     </div>
