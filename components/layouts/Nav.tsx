0341120e0181585c2850ecfed887ed9f6ec8b22e
Nguyễn Huy Hoàng <hoangnguyen.hn212.hn@gmail.com>
2022-11-11 22:08:08 +0700

    ADD login and signup

--- components/layouts/Nav.tsx
+++ components/layouts/Nav.tsx
@@ -1,13 +1,25 @@
 import Link from "next/link";
+import { supabase } from "../../supabase";
 
-export default function Nav() {
+export default async function Nav() {
+
+  const { data: { user } } = await supabase.auth.getUser()
+
   return (
     <header className="border-b border-neutral-700">
       <div className="mx-auto flex max-w-[1200px] items-center justify-between gap-3 px-5 py-6">
         <h1 className="text-3xl font-semibold">
           <Link href="/">Jmember</Link>
         </h1>
-        <Link href="/signup">Sign up</Link>
+        <div className="flex gap-5">
+          {
+            user &&
+            <>
+              <Link href="/login">Log in</Link>
+              <Link href="/signup">Sign up</Link>
+            </>
+          }
+        </div>
       </div>
     </header>
   );
