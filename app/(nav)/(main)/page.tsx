81ff28cc484781352b7224f43acd2fbd2231bdc1
prometheusalpha <hoangnguyen.hn212.hn@gmail.com>
2022-11-14 17:51:11 +0700

    ADD localsearch to replace broken router search

--- app/(nav)/(main)/page.tsx
+++ app/(nav)/(main)/page.tsx
@@ -1,8 +1,9 @@
 "use client";
 import AddLesson from "components/lessons/AddLesson";
 import LessonList from "components/lessons/LessonList";
+import LocalSearch from "components/search/LocalSearch";
 import RouterSearch from "components/search/RouterSearch";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { supabase } from "supabase";
 
 import { includeString } from "utils";
@@ -19,18 +20,29 @@ const fetchAllLessons = async () => {
 
 export default function Home() {
   const [lessons, setLessons] = useState([] as any[]);
-  fetchAllLessons().then((lessons) => setLessons(lessons));
-  // const searchResult = lessons.filter((lesson) =>
-  //   includeString(lesson.name, searchParams?.term ?? "")
-  // );
+  const [keyWord, setKeyWord] = useState("");
+  const [cardsSearch, setCardsSearch] = useState(lessons);
+
+  useEffect(() => {
+    fetchAllLessons().then((lessons) => {
+      setLessons(lessons);
+      setCardsSearch(lessons);
+    });
+  }, []);
+
+  useEffect(() => {
+    setCardsSearch(
+      lessons.filter((lesson) => includeString(lesson.name, keyWord))
+    );
+  }, [keyWord]);
 
   return (
     <div className="mx-auto grid max-w-[1200px] grid-cols-[repeat(auto-fill,minmax(350px,1fr))] gap-12 px-5 py-6">
       <div className="col-span-full flex justify-center">
-        <RouterSearch />
+        <LocalSearch setKeyWord={setKeyWord} />
       </div>
 
-      <LessonList lessons={lessons} />
+      <LessonList lessons={cardsSearch} />
       {lessons.length < 100 && <AddLesson />}
     </div>
   );
