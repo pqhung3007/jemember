a0aeceac9ba71968029918f5a7b5b0a7d799ed49
prometheusalpha <hoangnguyen.hn212.hn@gmail.com>
2022-11-15 16:21:30 +0700

    feat(nav): add logo animate on scroll

--- components/layouts/Nav.tsx
+++ components/layouts/Nav.tsx
@@ -25,13 +25,51 @@ export default function Nav() {
     router.push("/");
   };
 
+  let lastScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+  document.addEventListener(
+    "scroll",
+    () => {
+      let st = window.pageYOffset || document.documentElement.scrollTop;
+      let up = document.getElementById("nav-title-up");
+      let down = document.getElementById("nav-title-down");
+      if (st > lastScrollTop) {
+        // down
+        up?.classList.replace("right-0", "right-10");
+        up?.classList.replace("opacity-1", "opacity-0");
+        down?.classList.replace("left-0", "-left-[8.5rem]");
+        down?.classList.replace("opacity-0", "opacity-1");
+      } else {
+        // up
+        up?.classList.replace("right-10", "right-0");
+        up?.classList.replace("opacity-0", "opacity-1");
+        down?.classList.replace("-left-[8.5rem]", "left-0");
+        down?.classList.replace("opacity-1", "opacity-0");
+      }
+      lastScrollTop = st <= 0 ? 0 : st;
+    },
+    false
+  );
+
   return (
     <header className="fixed top-0 z-[99] w-full border-b border-gray-700 bg-gray-900">
       <div className="mx-auto flex max-w-[1200px] items-center justify-between gap-3 px-5 py-6">
         <h1 className="text-3xl font-semibold">
           <Link href="/" className="flex">
             <p className="text-red-500">J</p>
-            emember
+            <p
+              id="nav-title-up"
+              className="opacity-1 relative right-0 transition-all duration-300"
+            >
+              emember
+            </p>
+            <div
+              id="nav-title-down"
+              className="relative left-0 flex gap-2 opacity-0 transition-all duration-300"
+            >
+              <p className="text-red-500">S</p>
+              <p>Club</p>
+            </div>
           </Link>
         </h1>
         {!user.id && (
