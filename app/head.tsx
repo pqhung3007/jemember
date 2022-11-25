5f68e25ae20790314433cb64e78d4e6d38e071c2
Đỗ Phú Lộc <90917591+dwvn03@users.noreply.github.com>
2022-11-25 23:29:50 +0700

    fix: move SB Listener into nav

---
+++ app/head.tsx
@@ -0,0 +1,18 @@
+export default function RootHead() {
+  return (
+    <>
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Jemember" />
+      <meta
+        property="og:description"
+        content="Jemember is a flashcard app that helps you learn anything."
+      />
+      <meta property="og:url" content="https://jemember.jsclub.tech/" />
+      <meta property="og:site_name" content="Jemember" />
+      <meta property="og:image" content="/js_og.jpg" />
+      <meta property="og:image:width" content="480" />
+      <meta property="og:image:height" content="270" />
+      <title>Jemember</title>
+    </>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,12 @@
 import "styles/globals.css";
 
-import { useCurrentUserSession } from "utils/supabase/auth/server";
 import Nav from "components/layouts/Nav";
-import SupabaseListener from "components/common/supabase-listener";
-
-export const revalidate = 0;
 
 export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const { user, session } = await useCurrentUserSession();
-
   return (
     <html lang="en">
       <head>
@@ -20,23 +14,10 @@ export default async function RootLayout({
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" type="image/x-icon" href="/js.png" />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="Jemember" />
-        <meta
-          property="og:description"
-          content="Jemember is a flashcard app that helps you learn anything."
-        />
-        <meta property="og:url" content="https://jemember.jsclub.tech/" />
-        <meta property="og:site_name" content="Jemember" />
-        <meta property="og:image" content="/js_og.jpg" />
-        <meta property="og:image:width" content="480" />
-        <meta property="og:image:height" content="270" />
-        <title>Jemember</title>
       </head>
 
       <body className="text-neutral-100">
-        <Nav userID={user?.id} />
-        <SupabaseListener accessToken={session?.access_token} />
+        <Nav />
         <div className="min-h-screen bg-neutral-900">{children}</div>
       </body>
     </html>
